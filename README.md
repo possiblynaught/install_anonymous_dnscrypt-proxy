b3330b32d7c4c14a810ce968fb0bb2d52b154b1a
probablynaught <possiblynaught@pm.me>
2023-02-10 21:10:37 -0600

    overhaul to work with a variety of architectures

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # dnscrypt-proxy config generator
 
-Script to install dnscrypt-proxy2 on linux x86/64 targets, uses randomized Anonymous DNS and Oblivious DNS over HTTPS config by default.
+Script to install dnscrypt-proxy 2 on linux targets from latest release, uses randomized Anonymous DNS and Oblivious DNS over HTTPS config by default.
+# TODO: FINISH targets
 
 ## INFO
 
 This script will:
-1. Check for Linux x86_64 target
-2. Check for existing dnscrypt-proxy v1
-3. Check for existing dnscrypt-proxy v2
-4. Download dnscrypt-proxy2 tarball
+1. Check for Linux target architecture
+2. Detect target architecture
+3. Check for existing dnscrypt-proxy
+4. Download dnscrypt-proxy 2 tarball
 5. Build + install (statically linked) minisign if not installed and verify tarball with minisig. Skip this step by uncommenting ***SKIP_VERIFY=1***
 6. Generate the dnscrypt-proxy.toml config with Anonymous DNS + ODoH routes exclusively with [dnscrypt-proxy_config_generator](https://github.com/possiblynaught/dnscrypt-proxy_config_generator) submodule
 7. Makes a backup of /etc/resolv.conf -> /etc/resolv.conf.old and updates the file with the new *listen_addresses*
-8. Copy extracted dnscrypt binary/config to dir specified by variable: *INSTALL_LOCATION*, defaults to *$HOME/Documents/dnscrypt-proxy/*
-9. Installs and starts the dnscrypt-proxy service
-10. Tests DNS servers with [minimal_dnsleaktest](https://github.com/possiblynaught/minimal_dnsleaktest) submodule. Skip this step by uncommenting ***SKIP_DNSLEAKTEST=1***
+8. Copy extracted dnscrypt binary/config to dir specified by variable: *INSTALL_DIRECTORY*, defaults to */etc/dnscrypt-proxy/*
+9. Install, enable, and start the dnscrypt-proxy service
+10. Prompt user if they want to test DNS servers with the [minimal_dnsleaktest](https://github.com/possiblynaught/minimal_dnsleaktest) submodule.
 
 ## INSTALL
 
-To install and configure dnscrypt-proxy2, clone this repository, initialize the submodules, and trigger the install script:
+To install and configure dnscrypt-proxy 2, clone this repository, initialize the submodules, and trigger the install script:
 
 ```bash
 git clone https://github.com/possiblynaught/install_anonymous_dnscrypt-proxy.git
@@ -33,4 +34,4 @@ git submodule update
 - [x] Add minisign submodule
 - [x] Add DNS tester to end of script
 - [x] Add instructions
-- [ ] Create better install locations/perms
+- [x] Create better install locations/perms
--- install_dnscrypt.sh
+++ install_dnscrypt.sh
@@ -7,46 +7,65 @@ set -Eeo pipefail
 ################################################################################
 # Uncomment line to disable signiture verification and skip installing minisign
 # SKIP_VERIFY=1
-# Uncomment line to disable testing for dns leaks at end of install
-# SKIP_DNSLEAKTEST=1
-# Location where dnscrypt-proxy folder+binary will be installed
-INSTALL_LOCATION="$HOME/Documents"
+# Location where dnscrypt folder containing config+binary will be installed
+INSTALL_DIRECTORY="/etc/dnscrypt-proxy"
 ################################################################################
 
 # Save script dir
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 GEN_SCRIPT="$SCRIPT_DIR/dnscrypt-proxy_config_generator/generate_config.sh"
 
-# Check for linux version, expecting linux x86/64
-if ! uname -a | grep -q 'x86_64\|Linux'; then
-  echo "Error, x86_64 linux architecture not found"
+# TODO: Install from package repository if available
+# Make sure dnscrypt-proxy isn't already installed via package manager
+if command -v dnscrypt-proxy &> /dev/null; then
+  echo "Error, dnscrypt-proxy appears to be already installed via package manager, 
+  please remove it and run this script again."
+  exit 1
+fi
+
+# Function to check for a service name (arg $1) and notify if present
+check_service() {
+  local SERVICE="$1"
+  [ -n "$SERVICE" ] || (echo "Error, no service name passed to check_service()"; exit 1)
+  if ! command -v systemctl &> /dev/null; then
+    echo "Error, was expecting systemctl to be present for check_service()"
+    exit 1
+  fi
+  local STATUS
+  STATUS=$(systemctl status "$SERVICE" 2>&1 || true)
+  if ! echo "$STATUS" | grep -qF " could not be found."; then
+    echo "Error, existing service found for: $SERVICE"
+    echo "Please stop, disable, and remove the service:
+  systemctl stop $SERVICE
+  systemctl disable $SERVICE
+  sudo rm -f $(echo "$STATUS" | grep -F "Loaded: " | cut -d "(" -f 2 | cut -d ";" -f 1)
+  systemctl daemon-reload"
+    exit 1
+  fi
+}
+
+# Check for existing binary and services
+check_service "dnscrypt-proxy-resolvconf.service"
+check_service "dnscrypt-proxy.socket"
+check_service "dnscrypt-proxy.service"
+
+# Check for linux + architecture
+UARCH=$(uname -m)
+if ! uname -a | grep -qF "Linux"; then
+  echo "Error, please run this on a linux system"
+  exit 1
+elif [[ ! "${UARCH}" =~ arm|arm64|i386|mips|mips64|mips64le|mipsle|riscv64|x86_64 ]]; then
+  echo "Unsupported architecture: $UARCH"
   exit 1
 fi
 
 # Check for generate script
 if [[ ! -x "$GEN_SCRIPT" ]]; then
-  echo "Error, executable generate script not found: $GEN_SCRIPT
+  echo "Error, executable generate script not found:
+  $GEN_SCRIPT
 Make sure you have initialized the submodules with:
   git submodule init
   git submodule update"
-  exit 1
-fi
-
-# Make sure v1 isn't already installed
-if command -v dnscrypt-proxy &> /dev/null; then
-  echo "Error, dnscrypt-proxy appears to be already installed, please remove it and run this script again."
-  exit 1
-fi
-
-# Check for v2 or existing service
-EXISTING_SERVICE="/etc/systemd/system/dnscrypt-proxy.service"
-if [[ -f "$EXISTING_SERVICE" ]]; then
-  echo "Error, dnscrypt service aleady exists, please remove the existing service:
-  systemctl stop dnscrypt-proxy.service
-  systemctl disable dnscrypt-proxy.service
-  sudo rm $EXISTING_SERVICE
-
-  You might also need to reset or edit /etc/resolv.conf to reset dns"
   exit 1
 fi
 
@@ -54,15 +73,19 @@ fi
 DL_LOC="/tmp"
 TEMP_DOWNLOAD=$(mktemp /tmp/install_dnscrypt.XXXXXX || exit 1)
 # Get latest release links
-wget "https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest" -O "$TEMP_DOWNLOAD"
-DL_LINKS=$(grep -F "dnscrypt-proxy-linux_x86_64" < "$TEMP_DOWNLOAD")
+echo "Getting latest dnscrypt-proxy 2 release information..."
+wget -q "https://api.github.com/repos/DNSCrypt/dnscrypt-proxy/releases/latest" -O "$TEMP_DOWNLOAD" || \
+  (echo "Error downloading dnscrypt-proxy release, please check your internet connection or /etc/resolv.conf"; exit 1)
+DL_VERSION=$(grep -F "\"name\": \"Release" < "$TEMP_DOWNLOAD" | cut -d "\"" -f 4 | cut -d " " -f 2)
+DL_LINKS=$(grep -F "dnscrypt-proxy-linux_$UARCH" < "$TEMP_DOWNLOAD")
 DL_FILE="$DL_LOC/$(echo "$DL_LINKS" | grep -F "\"name\":" | grep -vF "minisig" | cut -d "\"" -f 4)"
 DL_SIG_FILE="$DL_LOC/$(echo "$DL_LINKS" | grep -F "\"name\":" | grep -F "minisig" | cut -d "\"" -f 4)"
 rm -f "$DL_FILE"
 rm -f "$DL_SIG_FILE"
 # Download releases
+echo "Downloading dnscrypt-proxy 2, version $DL_VERSION for $UARCH..."
 echo "$DL_LINKS" | grep -F "\"browser_download_url\":" | cut -d "\"" -f 4 | \
-  wget -i - -P "$DL_LOC"
+  wget -q -i - -P "$DL_LOC"
 rm -f "$TEMP_DOWNLOAD"
 
 # Verify files
@@ -71,13 +94,16 @@ if [[ ! "$SKIP_VERIFY" -eq 1 ]]; then
   if ! command -v minisign &> /dev/null; then
     INSTALL_MINISIGN="$SCRIPT_DIR/install_minisign/install_minisign.sh"
     if [[ ! -x "$INSTALL_MINISIGN" ]]; then
-      echo "Error, executable minisign install script not found: $INSTALL_MINISIGN
+      echo "Error, executable minisign install script not found:
+  $INSTALL_MINISIGN
 Make sure you have initialized the submodules with:
   git submodule init
   git submodule update"
       exit 1
     else
+      echo "--------------------------------------------------------------------------------"
       "${INSTALL_MINISIGN}"
+      echo
     fi
   fi
   # Check signature
@@ -87,14 +113,14 @@ Make sure you have initialized the submodules with:
 $SIG"
     exit 1
   else
-    echo "Signature for binary verified successfully:
+    echo "Signature for dnscrypt-proxy binary verified successfully:
   $DL_FILE"
   fi
 fi
 
 # Unpack
 EXTRACT_DIR="/tmp"
-UNPACK_DIR="$EXTRACT_DIR/linux-x86_64"
+UNPACK_DIR="$EXTRACT_DIR/linux-$UARCH"
 rm -rf "$UNPACK_DIR"
 mkdir -p "$EXTRACT_DIR"
 tar -xf "$DL_FILE" -C "$EXTRACT_DIR"
@@ -106,11 +132,13 @@ find "$UNPACK_DIR/." -type f -name 'example-*' -exec rm {} \;
 
 # Generate toml
 TOML_FILE="$UNPACK_DIR/dnscrypt-proxy.toml"
+echo "--------------------------------------------------------------------------------"
 "$GEN_SCRIPT" || true # TODO: Fix this
 mv "/tmp/dnscrypt-proxy.toml" "$TOML_FILE"
 # Pull dns listen address from toml
 LISTEN_ADDRESS=$(grep -F "listen_addresses = " < "$TOML_FILE" | grep -vF "#" | cut -d "'" -f 2 | cut -d ":" -f 1)
 [ -n "$LISTEN_ADDRESS" ] || (echo "Error, unable to get listen_address from: $TOML_FILE"; exit 1)
+
 # Check for and backup resolv.conf
 RESOLV_FILE="/etc/resolv.conf"
 OLD_RESOLV_FILE="/etc/resolv.conf.old"
@@ -125,13 +153,12 @@ echo "nameserver $LISTEN_ADDRESS
 options edns0
 #nameserver 8.8.8.8" | sudo tee -a "$RESOLV_FILE" > /dev/null
 
-# Install dnscrypt-proxy2
-FINAL_DIR="$INSTALL_LOCATION/dnscrypt-proxy"
-mkdir -p "$INSTALL_LOCATION"
-rm -rf "$FINAL_DIR"
-mv "$UNPACK_DIR" "$FINAL_DIR/"
+# Install dnscrypt-proxy 2
+mkdir -p "$(dirname "$INSTALL_DIRECTORY")"
+sudo rm -rf "$INSTALL_DIRECTORY"
+sudo mv "$UNPACK_DIR" "$INSTALL_DIRECTORY/"
 # Install service
-DNSCRYPT_BINARY="$FINAL_DIR/dnscrypt-proxy"
+DNSCRYPT_BINARY="$INSTALL_DIRECTORY/dnscrypt-proxy"
 [ -x "$DNSCRYPT_BINARY" ] || (echo "Error, dnscrypt binary not found in install location: $DNSCRYPT_BINARY"; exit 1)
 
 # Install service and run
@@ -144,15 +171,25 @@ systemctl status dnscrypt-proxy.service
 # Notify of completion
 echo "
 ################################################################################
-Installation completed to directory: $FINAL_DIR
+Installation completed to directory: $INSTALL_DIRECTORY
 The old version of the resolv file was saved to: $OLD_RESOLV_FILE
 If the network connection doesn't work after this script, reset resolv with:
   sudo mv $OLD_RESOLV_FILE $RESOLV_FILE
 ################################################################################"
-# Attempt dns leak test
-LEAK_SCRIPT="$SCRIPT_DIR/minimal_dnsleaktest/leaktest.sh"
-if [[ "$SKIP_DNSLEAKTEST" != 1 ]] && [ -x "$LEAK_SCRIPT" ]; then
-  echo -e "\nTesting DNS in 10 seconds..."
-  sleep 10
-  "${LEAK_SCRIPT}"
+
+# Ask about dns test
+read -p "Do you want to test your dns servers (Y/n)? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # Attempt dns leak test
+  DNS_SCRIPT="$SCRIPT_DIR/minimal_dnsleaktest/leaktest.sh"
+  if [ -x "$DNS_SCRIPT" ]; then
+    echo -e "\nTesting DNS servers in 10 seconds..."
+    sleep 10
+    "${DNS_SCRIPT}"
+  else
+    echo "Error, no executable dns script found: 
+  $DNS_SCRIPT"
+    exit 1
+  fi
 fi
